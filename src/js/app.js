8b5b9ba53ad9629c9b639a22d1c369e5d4a8e4c7
J <J@johnf-c02rt16tg8wm.t-mobile.com>
2016-11-05 22:56:59 -0700

    added loaded and interactive menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,85 @@
 /************************************
             APP.JS CORE
 ************************************/
+
+// Hides the loader bar, then the loader wrapper
+function hideLoader(){
+	// Remove loader
+	var loaderWrapper = document.getElementsByClassName('loader-wrapper')[0];
+	setTimeout(function(){
+
+		var loader = loaderWrapper.getElementsByClassName('loader')[0];
+		loader.outerHTML = "";
+		delete loader;
+
+		loaderWrapper.classList.add('hidden')
+			setTimeout(function(){
+			loaderWrapper.outerHTML = "";
+			delete loader;
+		}, 500)
+
+	}, 500)
+
+}
+
+// Typically, clicking the category menu takes you to the 'projects' page
+function initMenu(){
+	var links = document.getElementsByClassName('category-link')
+	for( var i = 0; i < links.length; i ++){
+		var navCategory = links[i];
+		navCategory.addEventListener("click", function(e){
+			var target = $(this).data('target');
+			window.location.href = '/projects.html#'+target;
+		})
+	}	
+}
+
+// On the 'projects' page itself, the menu doesn't reload the page.
+// It merely changes which category is visible.
+function initCategoryMenu(){
+	var links = document.getElementsByClassName('category-link')
+	for( var i = 0; i < links.length; i ++){
+		var navCategory = links[i];
+		navCategory.addEventListener("click", function(e){
+			var target = $(this).data('target');
+			initCategories(target)
+			window.location.href = '/projects.html#'+target;
+		})
+	}	
+}
+
+// Open the category in the URL hash. 
+// If no category is in the hash, open the first category
+function initCategories(target){
+	var categories = document.getElementsByClassName('category')
+
+	for( var i = 0; i < categories.length; i ++){
+		var category = categories[i];
+		category.classList.remove('active');
+	}
+
+	if (target == ''){
+		var categories = document.getElementsByClassName('category')
+		target = categories[0].className.split(' ')[1];
+		window.location.hash = "#"+target
+	}
+		
+	var active = document.getElementsByClassName(target)
+
+	for( var i = 0; i < active.length; i ++){
+		active[i].classList.add('active');
+	}	
+}
+
 $(document).ready(function(){
-  console.log('Yeay, it works!');
+	hideLoader()
+
+	if(window.location.pathname === "/projects.html"){
+		// On the projects page, open the menu to the category in the url hash
+		var target = window.location.hash.replace("#",'')
+		initCategories(target)
+		initCategoryMenu()
+	} else {
+		initMenu()
+	}
 });
