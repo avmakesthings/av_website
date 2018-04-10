ace437bb7aca67477830712db5df0159b79bb367
AV <av@anastasia.io>
2018-04-10 16:56:02 -0700

    masonry bug fix for grid item overlap on load

--- src/js/app.js
+++ src/js/app.js
@@ -50,9 +50,14 @@ function initCategoryMenu(){
 
 function initMasonry(){
 	// Assumes masonry has been included
-	$('.grid').masonry({
+	var $grid = $('.grid').masonry({
 		itemSelector: '.grid-item',
 	});
+	// layout Masonry after each image loads
+	$grid.imagesLoaded().progress( function() {
+		$grid.masonry('layout');
+  	});
+
 }
 
 
